c490716ca71e18c03f03b8f721eaf5b9e04dc969
toby.cantello@yum.com <toby.cantello@yum.com>
2023-07-27 17:08:31 -0500

    each player gets own draw
    fixed the code to where each player draws one card at a time from desk. added the value and suite of each card as drawn. Once all 52 cards are played the Battle button changes to ask if they want to play again.

--- app.py
+++ app.py
@@ -1,33 +1,47 @@
 """
 created by: Toby Cantello
 Date created: 7/26/23
-Last updated: 7/26/23
+Last updated: 7/27/23
 """ 
 
 # Import required modules
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 import requests
 import json
 
 # Flask Object creation
 app = Flask(__name__)
 
+# Setting Secert Key 
+app.secret_key = 'YumRocks2023!'
+
 # Routes
 @app.route("/", methods=["POST", "GET"])
 def index():
+
+    # Request is made to the API for a new deck of cards
+    deck_req = requests.get('https://deckofcardsapi.com/api/deck/new/shuffle')
+    deck = json.loads(deck_req.content)
+    deckId = deck['deck_id']
+    session['deckId'] = deckId
     return render_template("index.html")
 
 @app.route("/play", methods=["POST", "GET"])
 def play():
-    deck_req = requests.get('https://deckofcardsapi.com/api/deck/new/shuffle')
-    deck = json.loads(deck_req.content)
-    deckId = deck['deck_id']
-    card_req = f'https://deckofcardsapi.com/api/deck/{deckId}/draw/?count=1'
-    card_select = requests.get(card_req)
-    card = json.loads(card_select.content)
-    print(card)
-   
-    return render_template("play.html", deck=deck, card=card, cards=card['cards'])
+
+    # Gets the Deck Id that was created when "/" route was accessed
+    deckId = session.get('deckId', None)
+
+    # Player 1 draws a card
+    p1_card_req = f'https://deckofcardsapi.com/api/deck/{deckId}/draw/?count=1'
+    p1_card_select = requests.get(p1_card_req)
+    p1_card = json.loads(p1_card_select.content)
+
+    # Player 2 draws a card
+    p2_card_req = f'https://deckofcardsapi.com/api/deck/{deckId}/draw/?count=1'
+    p2_card_select = requests.get(p2_card_req)
+    p2_card = json.loads(p2_card_select.content)
+    return render_template("play.html", p1_card=p1_card, p1_cards=p1_card['cards'], p2_card=p2_card, p2_cards=p2_card['cards'])
 
 # Starts the server
 if __name__ == "__main__":
